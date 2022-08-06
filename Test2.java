335a35d62365c1e70e96e61419198be176cfec17
Tests77 <110728361+Tests77@users.noreply.github.com>
2022-08-06 16:20:08 +0300

    Add files via upload

---
+++ Test2.java
@@ -0,0 +1,15 @@
+
+public class Test2 {
+
+	public static void main(String[] args) 
+	  {
+	    {
+	      int x = 0;
+	      
+	    }
+	    while(true) {
+	    String x = "6,7";
+	    System.out.print(x+",");
+	  }
+	  }
+}
